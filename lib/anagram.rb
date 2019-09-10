381095e96794767d64e5658fcf55a27cf8363176
github username <github email address>
2019-09-09 20:10:03 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :words
+
+    def initialize(words)
+        @words = words
+    end
+
+    def match(twister)
+        twister.select {|single| single.split("").sort == @words.split("").sort}
+    end
+
+
+end
